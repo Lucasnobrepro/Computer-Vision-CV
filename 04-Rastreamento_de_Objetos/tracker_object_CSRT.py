3d44dfe058c4b5c6515892b424e71fe6e1ed1ec1
lucasnobrepro <lucasnobrepro2015@gmail.com>
2021-04-30 14:43:03 -0300

    add tracking object video

---
+++ 04-Rastreamento_de_Objetos/tracker_object_CSRT.py
@@ -0,0 +1,44 @@
+import cv2
+
+# Criando rastreador;
+tracker = cv2.TrackerCSRT_create()
+
+# Capturando video;
+video = cv2.VideoCapture("")
+
+# Lendo video
+ok, frame = video.read()
+
+# Selecionando bounding box do objeto a ser rastreado;
+bbox = cv2.selectROI(frame)
+
+# Para ler todos os frames do video;
+while True:
+    ok, frame = video.read()
+
+    # Se o video terminar sai do while;
+    if not ok:
+        break
+
+    # Atualizando bbox;
+    ok, bbox = tracker.update(frame)
+
+    if ok:
+        # Pegando informação de rastreio;
+        (x, y, w, h) = [int(v) for v in bbox]
+        
+        # Desenha um retangulo verdr ao redor do objeto rastreado;
+        cv2.rectangle(frame, # Imagem que será desenhada;
+                     (x,y), (x+w, y+h), # Posição que será desenhado o retangulo;
+                     (0,255,0)) # Cor do retangulo está em BGR;
+    else:
+        # Adicona o texto caso não possa mais fazer o rastreamento do objeto;
+        cv2.putText(frame, "Fail in tracking..", (100,80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)           
+
+    # Plota a imagem
+    cv2.imshow("Tracking", frame)
+    # fecha o ploty com o botão ESC;
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
